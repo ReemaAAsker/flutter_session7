8fff4838de5a847340be49b170dde11d5f01d268
ReemaAAsker <reemaaliasker@gmail.com>
2025-08-02 11:21:56 +0300

    super class example is done

---
+++ person.dart
@@ -0,0 +1,47 @@
+import 'auth_interface.dart';
+import 'sch_interface.dart';
+
+abstract class Person implements Auth, Schedualable {
+  String? name;
+  int? age;
+  Person({this.name, this.age});
+
+  String displayInfo() {
+    return "Name: $name, Age: $age";
+  }
+
+  void printRole() {
+    // concrete functiob
+    print("Role is Person");
+  }
+
+  void describeRole(); // abstract function
+}
+/*
+functions >> concrete functions   , abstract functions
+*/
+
+/*
+Student is a person
+Teacher is a person
+-------------------------------
+   Shapes (color , area)
+
+ rectangle is a shape
+ circle is a shape
+ Triangle is a shape
+--------------------------------
+Touota is a  Car
+Marsedis  is a Car
+
+----------------
+
+is-a   >> Inhritance
+  
+
+  Inhritance type :
+   Single inhritance
+   Multiple inhritance
+   Hybrid inhritance
+   Multilevel inhritance
+*/
